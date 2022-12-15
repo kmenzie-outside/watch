71d0be77dda2b9c1ebb1bede76092010ed725c32
Seth Lowenstern <slowenstern@outsideinc.com>
2022-12-15 13:51:05 -0500

    Working with updating cast

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import './App.css';
 import Movie from './components/Movie';
-import ReactJWPlayer from 'react-jw-player';
 
 function App() {
     return (
         <div className="App">
-            <Movie>
-                <ReactJWPlayer
-                    playerId='NjqDlXWT'
-                    playerScript='https://cdn.jwplayer.com/libraries/NjqDlXWT.js'
-                    file='https://cdn.jwplayer.com/videos/5XNxzP2U-MJjSqWn0.mp4'
-
-                    //onAll={(event) => console.log(event)}
-                />
-            </Movie>
+            <Movie
+                playerId='NjqDlXWT'
+                playerScript='https://cdn.jwplayer.com/libraries/NjqDlXWT.js'
+                file='https://cdn.jwplayer.com/videos/5XNxzP2U-MJjSqWn0.mp4'
+                isAutoPlay={true}
+            />
         </div>
     );
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import ReactJWPlayer from 'react-jw-player';
 import './Movie.css';
 const webvtt = require('node-webvtt');
@@ -7,73 +7,71 @@ export default function Movie(props) {
 
     const [Visible, setVisible] = useState(false);
     const [cast, setCast] = useState([]);
+    const [activeCast, setActiveCast] = useState([]);
     const [seconds, setSeconds] = useState(0);
 
     useEffect(() => {
+        const getCast = async () => {
+            //Get Video File
+            const response = await fetch('https://cdn.jwplayer.com/v2/media/5XNxzP2U');
+            const json = await response.json();
+            const trackInfo = json.playlist[0].tracks;
+            trackInfo.splice(trackInfo.length - 1);
 
-        //Get Video File
-        const response = await fetch('https://cdn.jwplayer.com/v2/media/5XNxzP2U');
-        const json = await response.json();
-        const trackInfo = json.playlist[0].tracks;
-        trackInfo.splice(trackInfo.length - 1);
-
-        //Loop through tracks in file
-        const curCast = trackInfo.map(data => {
-            const res = await fetch(data.file);
-            const parsed = webvtt.parse(await res.text());
-            return { name: data.label, cues: parsed.cues };
-
-        });
-        setCast(curCast);
-
+            //Loop through tracks in file
+            const castList = await Promise.all(
+                trackInfo.map(async (data) => {
+                    const res = await fetch(data.file);
+                    const parsed = webvtt.parse(await res.text());
+                    return { name: data.label, cues: parsed.cues };
+                })
+            );
+            setCast(castList);
+            setActiveCast(castList);
+        };
+        getCast().catch(console.error);
     }, []);
 
     const updateTime = (time) => {
         setSeconds(Math.round(time.currentTime));
+        updateCast(seconds);
     };
-    // We'll use this funtion to see who should be on screen when
 
-
-    const updateCast = () => {
-
-        console.log(seconds);
-        //   const second = Math.round(time.currentTime);
-        //   // Eventually, this "if" needs to look at a dynamic source or JSON file
-
-        cast.map((data) => {
-            const queueArray = data[0].cues;
-            console.log(queueArray);
-            //queuArray.map((queueData) => {
-            //console.log(queueData) 
-            //if(10 > queueData.start) { console.log('yes!') }
-            //})
-
+    const updateCast = useCallback((secondsIn) => {
+        const active = cast.filter((actor) => {
+            for (let i = 0; i < actor.cues.length; i++) {
+                if (actor.cues[i].start > secondsIn) {
+                    return false;
+                }
+                else if (actor.cues[i].start <= secondsIn && actor.cues[i].end >= secondsIn) {
+                    return true;
+                }
+            }
+            return false;
         });
-
-
-        //   if(second === 5) {
-        //     setCast([
-        //       {name: "Bode Miller", id: 1}
-        //     ]
-        //     )
-        //   }
-        setVisible(true);
-    };
-
-    const jwPlayer = React.Children.only(this.props.Children);
-    jwPlayer.onTime = (time) => updateTime(time);
-    jwPlayer.isAutoPlay = true;
-    jwPlayer.onPause = updateCast;
-    jwPlayer.onResume = () => setVisible(false);
+        setActiveCast(active);
+    },
+        [seconds]
+    );
 
     return (
         <div className="reactVideo">
 
             <div className={Visible ? 'fadeIn' : 'fadeOut'}>
-                <CastDisplay cast={cast}></CastDisplay>
+                <CastDisplay cast={activeCast}></CastDisplay>
             </div>
+            <ReactJWPlayer
+                playerId={props.playerId}
+                playerScript={props.playerScript}
+                file={props.file}
+                isAutoPlay={props.isAutoPlay}
 
-            {jwPlayer}
+
+                //onAll={(event) => console.log(event)}
+                onTime={(time) => updateTime(time)}
+                onPause={() => setVisible(true)}
+                onResume={() => setVisible(false)}
+            />
 
             <button onClick={() => console.log(cast)}>Get Chapters</button>
         </div>
@@ -81,18 +79,21 @@ export default function Movie(props) {
 }
 
 function CastDisplay(props) {
-    return (
-        <div className="cast">
-            {props.cast.map((castInfo, index) => (
-                <div className="castDetails" key={index}>
-                    <img src={"/assets/" + castInfo[0].name + ".png"}
-                        alt="{castInfo.name}"
-                        className="castImage" />
-                    <h2>{castInfo[0].name}</h2>
-                </div>
+    if (props.cast?.length > 0) {
+        return (
+            <div className="cast">
+                {props.cast.map((castInfo, index) => (
+                    <div className="castDetails" key={index}>
+                        <img src={"/assets/" + castInfo.name + ".png"}
+                            alt="{castInfo.name}"
+                            className="castImage" />
+                        <h2>{castInfo.name}</h2>
+                    </div>
 
-            ))}
+                ))}
 
-        </div>
-    );
+            </div>);
+        }
+    return null;
+
 }
