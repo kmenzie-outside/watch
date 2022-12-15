7518011a29f9540802863ca3a9267342b3ecd090
Seth Lowenstern <slowenstern@outsideinc.com>
2022-12-15 17:00:41 -0500

    Add initial focused detail view

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import ReactJWPlayer from 'react-jw-player';
 import './Movie.css';
 const webvtt = require('node-webvtt');
@@ -32,18 +32,13 @@ export default function Movie(props) {
         getCast().catch(console.error);
     }, []);
 
-    const updateTime = (time) => {
-        setSeconds(Math.round(time.currentTime));
-        updateCast(seconds);
-    };
-
     const updateCast = useCallback((secondsIn) => {
         const active = cast.filter((actor) => {
             for (let i = 0; i < actor.cues.length; i++) {
                 if (actor.cues[i].start > secondsIn) {
                     return false;
                 }
-                else if (actor.cues[i].start <= secondsIn && actor.cues[i].end >= secondsIn) {
+                else if (actor.cues[i].text === "In" && actor.cues[i].start <= secondsIn && actor.cues[i].end >= secondsIn) {
                     return true;
                 }
             }
@@ -51,49 +46,93 @@ export default function Movie(props) {
         });
         setActiveCast(active);
     },
-        [seconds]
+        [cast]
     );
+
+    const updateTime = (time) => {
+        setSeconds(Math.round(time.currentTime));
+        updateCast(seconds);
+    };
 
     return (
         <div className="reactVideo">
 
-            <div className={Visible ? 'fadeIn' : 'fadeOut'}>
-                <CastDisplay cast={activeCast}></CastDisplay>
-            </div>
+
             <ReactJWPlayer
                 playerId={props.playerId}
                 playerScript={props.playerScript}
                 file={props.file}
                 isAutoPlay={props.isAutoPlay}
 
-
-                //onAll={(event) => console.log(event)}
                 onTime={(time) => updateTime(time)}
                 onPause={() => setVisible(true)}
                 onResume={() => setVisible(false)}
             />
-
-            <button onClick={() => console.log(cast)}>Get Chapters</button>
+            <CastDisplay cast={activeCast} visible={Visible} />
         </div>
     );
 }
 
 function CastDisplay(props) {
-    if (props.cast?.length > 0) {
-        return (
-            <div className="cast">
-                {props.cast.map((castInfo, index) => (
-                    <div className="castDetails" key={index}>
-                        <img src={"/assets/" + castInfo.name + ".png"}
-                            alt="{castInfo.name}"
-                            className="castImage" />
-                        <h2>{castInfo.name}</h2>
-                    </div>
-
-                ))}
-
-            </div>);
+    const [animationActive, setAnimationActive] = useState(false);
+    const [focusedActor, setFocusedActor] = useState(null);
+    useEffect(() => {
+        if (!props.visible) {
+            setFocusedActor(null);
         }
-    return null;
+    },[props.visible]);
 
+
+    const focusActor = (toFocus) => {
+        if (toFocus === focusedActor) {
+            setFocusedActor(null);
+        }
+        else {
+            setFocusedActor(toFocus);
+        }
+    };
+
+    return (
+        <div className={'castContainer ' + (props.visible ? 'fadeIn' : 'fadeOut')}
+            onAnimationEnd={() => setAnimationActive(false)}
+            onAnimationStart={() => setAnimationActive(true)}>
+            {((props.visible || animationActive) && props.cast?.length > 0) ?
+                <>
+                    <FocusedPerson focused={focusedActor} />
+                    <div className="cast">
+                        {props.cast.map((castInfo, index) => (
+                            <CastMemberTile castInfo={castInfo} key={index} focusActor={focusActor} />
+                        ))}
+                    </div>
+                </>
+                : null}
+        </div>
+    );
+}
+
+function FocusedPerson(props) {
+    if (props.focused) {
+        return (
+            <article>
+                <img src={"/assets/" + props.focused.name + ".png"}
+                    alt="{props.focused.name}"
+                    className="castImage" />
+                <h1>{props.focused.name}</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+            </article>
+        );
+    }
+    return null;
+}
+
+function CastMemberTile(props) {
+
+    return (
+        <div className="castDetails"
+            onClick={() => props.focusActor(props.castInfo)}>
+            <img src={"/assets/" + props.castInfo.name + ".png"}
+                alt="{props.castInfo.name}"
+                className="castImage" />
+            <h2>{props.castInfo.name}</h2>
+        </div>);
 }
